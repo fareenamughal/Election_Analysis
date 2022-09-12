521c60bd0887cae8baab7d42ad33191b07200499
fareenamughal <112118706+fareenamughal@users.noreply.github.com>
2022-09-11 23:51:33 -0400

    Add files via upload

---
+++ Election_Analysis/Python_practice_workbook1.py
@@ -0,0 +1,7 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+counties.insert(2, "El Paso")
+counties.remove("Arapahoe")
+counties.remove("Denver")
+counties.insert(3, "Denver")
+counties.append("Arapahoe"
+print(counties)
