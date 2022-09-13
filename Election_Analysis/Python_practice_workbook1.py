178b0ec4cccbab06e4dba35c255525559c4f6efa
fareenamughal <112118706+fareenamughal@users.noreply.github.com>
2022-09-13 00:31:25 -0400

    Delete Python_practice_workbook1.py

--- Election_Analysis/Python_practice_workbook1.py
+++
@@ -1,7 +0,0 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-counties.insert(2, "El Paso")
-counties.remove("Arapahoe")
-counties.remove("Denver")
-counties.insert(3, "Denver")
-counties.append("Arapahoe"
-print(counties)
